ea969d58cdd18c1bcfc1af377a527961ec83035a
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2022-08-27 12:57:41 +0530

    Delete etc/calamares directory

--- etc/calamares/branding/matsya/test-slides.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-
-## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
-
-## Test Calamares Slides
-qmlscene -I /usr/share/calamares/qml show.qml -geometry 825x500
